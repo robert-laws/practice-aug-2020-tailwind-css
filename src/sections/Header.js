a19e33cf01f3c6823b6fd8a840211932cd7fc50a
Robert Laws <roblaws@me.com>
2020-09-04 15:07:21 +0300

    update header with logo link

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -4,11 +4,12 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <header className='flex flex-row items-center px-10 bg-gray-200 h-16'>
-      <div className='logo flex-initial'>Logo</div>
-      <nav className='flex-auto flex justify-end'>
+      <div className='logo flex-initial'>
         <NavLink className='mx-6 hover:text-red-400' exact to='/'>
-          Home
+          Logo - Home
         </NavLink>
+      </div>
+      <nav className='flex-auto flex justify-end'>
         <NavLink className='mx-6 hover:text-red-400' to='todos'>
           To Do Lists
         </NavLink>
