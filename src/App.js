ba2a6e75710438a1076c0cc84569605be9396fac
Robert Laws <roblaws@me.com>
2020-09-04 14:36:37 +0300

    add pages and basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import TodoLists from './pages/TodoLists';
+import Profile from './pages/Profile';
+import Keywords from './pages/Keywords';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className='App'>
-      <h1 className='main-title'>App</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <header>header</header>
+        <main>
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path='/todos' component={TodoLists} />
+            <Route path='/profile' component={Profile} />
+            <Route path='/keywords' component={Keywords} />
+            <Route path='/about' component={About} />
+            <Route path='*' component={NotFound} />
+          </Switch>
+        </main>
+        <footer>footer</footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/TodoLists.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoLists = () => {
+  return <div>To Do Lists</div>;
+};
+
+export default TodoLists;
